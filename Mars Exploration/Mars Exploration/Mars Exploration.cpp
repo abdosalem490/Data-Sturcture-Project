bc827081fa01a665bf2c0199fec5ec8aa5cd88f4
Ahmed-H <76230133+Ahmed-Hany-F@users.noreply.github.com>
2021-05-02 17:48:49 +0200

    linkedqueue

---
+++ Mars Exploration/Mars Exploration/Mars Exploration.cpp
@@ -0,0 +1,11 @@
+// Mars Exploration.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "LinkedQueue.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
