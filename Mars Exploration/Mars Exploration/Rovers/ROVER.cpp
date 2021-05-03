93a24b0df0f6d854f8ee7cad0d1bf559187bfe5b
yusufsaeed <yousefrabea74@gmail.com>
2021-05-03 19:10:56 +0200

    Rover class

---
+++ Mars Exploration/Mars Exploration/Rovers/ROVER.cpp
@@ -0,0 +1,59 @@
+#include "ROVER.h"
+
+Rover::Rover(std::string argType, double argSpeed = 0) {
+
+	assignMission(nullptr);
+	setSpeed(argSpeed);
+	setType(argType);
+
+}
+
+void Rover::assignMission(int* a) {
+	assignedMission = a;
+}
+
+void Rover::setSpeed(double argSpeed) {
+	speed = argSpeed;
+}
+double Rover::getSpeed() const {
+	return speed;
+}
+
+void Rover::setCheckupD(int CD) {
+	checkupDuration = CD;
+}
+int Rover::getCheckupD() const {
+	return checkupDuration;
+}
+
+void Rover::incrementCM() {
+	completedMissions++;
+}
+void Rover::decrementCM() {
+	if (completedMissions != 0)
+		completedMissions--;
+}
+int Rover::getCM() const {
+	return completedMissions;
+}
+
+void Rover::incrementFM() {
+	failedMissions++;
+}
+int Rover::getFM() const {
+	return failedMissions;
+}
+
+std::string Rover::getType() const {
+	return type;
+}
+void Rover::setType(std::string t) {
+	type = t;
+}
+
+int Rover::getStatus() const {
+	if (assignedMission == nullptr)
+		return 1; // Available
+	else
+		return 0; // Not available.
+}
---
+++ Mars Exploration/Mars Exploration/Rovers/ROVER.h
@@ -0,0 +1,61 @@
+#ifndef _ROVER_H
+#define _ROVER_H
+#include <string>
+
+class Rover {
+
+public:
+
+
+	Rover(std::string, double);
+
+	// Setters and Getters for speed.
+	void setSpeed(double);
+	double getSpeed() const;
+
+	// Setters and Getters for Checkup Duration.
+	void setCheckupD(int CD);
+	int getCheckupD() const;
+
+	// Get No. completed missions, increment it, 
+	// or decrement it (in case of faliure for instance.)
+	int getCM() const;
+	void incrementCM();
+	void decrementCM();
+
+	// Get No. failed missions, increment it.
+	int getFM() const;
+	void incrementFM();
+
+	std::string getType() const;
+	void setType(std::string);
+
+	// in the following : replace int pointer with a mission pointer
+	// when the classes are ready.
+	void assignMission(int*);
+
+
+	// This may be needed or may be not, depending on how we further implement the project.
+	int getStatus() const;
+
+private:
+
+	double speed;
+	std::string type;
+	int checkupDuration;
+
+
+	int completedMissions;
+	// For bonus, to store the number of failed missions that were assigned to this rover.
+	// This may help then to determine the parameter of when to move this rover to maintainance.
+	int failedMissions;
+
+	// replace the int pointer with a mission pointer!
+	int* assignedMission;
+
+
+};
+
+
+
+#endif
